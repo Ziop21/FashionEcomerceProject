523b007a6e6abf1b72c4493786e96b9fd132e14d
npp2107 <142470779+npp2107@users.noreply.github.com>
2023-08-31 16:27:48 +0700

    init

---
+++ src/main/java/project/fashionecomerce/backend/fashionecomerceproject/FashionEcomerceProjectApplication.java
@@ -0,0 +1,13 @@
+package project.fashionecomerce.backend.fashionecomerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionEcomerceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionEcomerceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/fashionecomerce/backend/fashionecomerceproject/FashionEcomerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.fashionecomerce.backend.fashionecomerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionEcomerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
