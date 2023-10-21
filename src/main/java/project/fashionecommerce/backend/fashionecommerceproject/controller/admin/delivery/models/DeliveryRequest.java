57ac0241062e8f6242fb7fe105272a1d8fe7d269
npp2107 <142470779+npp2107@users.noreply.github.com>
2023-10-21 22:23:40 +0700

    CRUD delivery

--- src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/admin/delivery/models/DeliveryRequest.java
+++ src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/admin/delivery/models/DeliveryRequest.java
@@ -1,6 +1,9 @@
 package project.fashionecommerce.backend.fashionecommerceproject.controller.admin.delivery.models;
 
+import jakarta.validation.constraints.NotNull;
+
 public record DeliveryRequest(
+        @NotNull
         String name,
         String description
 ) {
--- src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/admin/size/models/SizeRequest.java
+++ src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/admin/size/models/SizeRequest.java
@@ -1,6 +1,9 @@
 package project.fashionecommerce.backend.fashionecommerceproject.controller.admin.size.models;
 
+import jakarta.validation.constraints.NotNull;
+
 public record SizeRequest(
+        @NotNull
         String name
 ) {
 }
