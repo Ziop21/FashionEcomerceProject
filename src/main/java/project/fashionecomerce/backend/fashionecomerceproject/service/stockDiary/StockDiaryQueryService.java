8461124dbb1a50dea54a4f49d33ddd55254f4c80
hoangtrannguyen <hoangtnguyen17@gmail.com>
2023-10-21 22:55:24 +0700

    CRUD StockDiary

--- src/main/java/project/fashionecomerce/backend/fashionecomerceproject/service/stockDiary/StockDiaryQueryService.java
+++
@@ -1,9 +0,0 @@
-package project.fashionecomerce.backend.fashionecomerceproject.service.stockDiary;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class StockDiaryQueryService {
-}
