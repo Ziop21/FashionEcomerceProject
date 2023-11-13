f62175002753e27fb3987615526a0512ad7c7af9
npp2107 <142470779+npp2107@users.noreply.github.com>
2023-11-13 21:26:39 +0700

    CRUD staff-stock

--- src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/staff/stock/StaffStocksController.java
+++ src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/staff/stock/StaffStocksController.java
@@ -8,15 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import project.fashionecommerce.backend.fashionecommerceproject.common.models.PageResponse;
-import project.fashionecommerce.backend.fashionecommerceproject.controller.admin.stock.models.StockModelMapper;
-import project.fashionecommerce.backend.fashionecommerceproject.controller.admin.stock.models.StockResponse;
 import project.fashionecommerce.backend.fashionecommerceproject.controller.staff.stock.models.StaffStockModelMapper;
 import project.fashionecommerce.backend.fashionecommerceproject.controller.staff.stock.models.StaffStockRequest;
 import project.fashionecommerce.backend.fashionecommerceproject.controller.staff.stock.models.StaffStockResponse;
 import project.fashionecommerce.backend.fashionecommerceproject.dto.handler.MySortHandler;
 import project.fashionecommerce.backend.fashionecommerceproject.dto.stock.Stock;
 import project.fashionecommerce.backend.fashionecommerceproject.dto.stock.StockQuery;
-import project.fashionecommerce.backend.fashionecommerceproject.service.database.stock.StockUseCaseService;
 import project.fashionecommerce.backend.fashionecommerceproject.service.staff.StaffStockUseCaseService;
 
 import java.util.List;
--- src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/staff/stock/models/StaffStockResponse.java
+++ src/main/java/project/fashionecommerce/backend/fashionecommerceproject/controller/staff/stock/models/StaffStockResponse.java
@@ -1,9 +1,6 @@
 package project.fashionecommerce.backend.fashionecommerceproject.controller.staff.stock.models;
 
-import project.fashionecommerce.backend.fashionecommerceproject.controller.admin.review.models.ReviewResponse;
-
 import java.time.LocalDateTime;
-import java.util.List;
 
 public record StaffStockResponse(
         String id,
